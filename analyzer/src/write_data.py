3a7e6c76bed73f01261d4b8ab7e666d342f9cef7
Alan Covarrubias <alan.cov@icloud.com>
2021-01-05 16:47:00 -0800

    add data dir

--- analyzer/src/write_data.py
+++ analyzer/src/write_data.py
@@ -1,14 +1,17 @@
 import requests
 import json
+import os
 
+
+os.mkdir('data')
 games_res = requests.get("http://nba:3001/seasons/1/games?team=1&player=1")
 games_data = json.loads(games_res.text)
 
-with open("games.json", "w") as outfile:
+with open("data/games.json", "w") as outfile:
     json.dump(games_data, outfile)
 
 game_res = requests.get("http://nba:3001/games/1?team=1&player=1")
 game_data = json.loads(game_res.text)
 
-with open("game.json", "w") as outfile:
+with open("data/game.json", "w") as outfile:
     json.dump(game_data, outfile)
