5d002b8a8455afc9daad9c6a0cba921da32a7144
Alan Covarrubias <alan.cov@icloud.com>
2021-01-08 17:20:08 -0800

    refactor

--- analyzer/src/analyzer.py
+++ analyzer/src/analyzer.py
@@ -10,9 +10,9 @@ def predict_score(team, opponent):
     season_stat = NbaTeamStat(team.season_stat, opponent.season_stat)
     previous_stat = NbaTeamStat(team.previous_stat, opponent.previous_stat)
     ortg = (season_stat.ortg + previous_stat.ortg) / 2
-    tot_poss = (
-        season_stat.tot_poss / team.count + previous_stat.tot_poss / team.previous_size
-    ) / 2
+    season_tot_poss = season_stat.tot_poss / team.count
+    previous_tot_poss = previous_stat.tot_poss / team.previous_size
+    tot_poss = (season_tot_poss + previous_tot_poss) / 2
     return tot_poss * ortg / 100
 
 
@@ -42,10 +42,9 @@ class Analyzer:
             if not away_team or not home_team:
                 self.add_game_data(game)
                 continue
-            enough_previous_stats = (
-                len(away_team.previous_stats) == self.previous_length
-                and len(home_team.previous_stats) == self.previous_length
-            )
+            enough_away_stats = len(away_team.previous_stats) == self.previous_length
+            enough_home_stats = len(home_team.previous_stats) == self.previous_length
+            enough_previous_stats = enough_away_stats and enough_home_stats
             if not enough_previous_stats:
                 self.add_game_data(game)
                 continue
@@ -68,10 +67,8 @@ class Analyzer:
 
     def count(self, total, actual, prediction, counter):
         if abs(total - prediction) > self.diff:
-            if prediction < total:
-                counter.win() if actual < total else counter.lose()
-            else:
-                counter.win() if actual > total else counter.lose()
+            win = actual < total if prediction < total else actual > total
+            counter.win() if win else counter.lose()
         else:
             counter.skip()
 
