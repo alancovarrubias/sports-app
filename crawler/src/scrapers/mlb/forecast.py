dfcc9e71363a3151fcaeb3c6e7001ac2cc733f13
Alan Covarrubias <alan.cov@icloud.com>
2021-07-30 08:19:24 -0700

    add team endpoints

--- crawler/src/scrapers/mlb/forecast.py
+++ crawler/src/scrapers/mlb/forecast.py
@@ -2,9 +2,48 @@ from scrapers.wunderground import WundergroundScraper
 from models.mlb.forecast import MlbForecast
 
 
+def get_team_endpoint(team):
+    wunderground_dict = {
+        "LAA": "ca/anaheim/92806",
+        "BAL": "md/baltimore/21201",
+        "BOS": "ma/boston/02215",
+        "CHW": "il/chicago/60616",
+        "CLE": "oh/cleveland/44115",
+        "DET": "mi/detroit/48201",
+        "KCR": "mo/kansas-city/64129",
+        "MIN": "mn/minneapolis/55403",
+        "NYY": "ny/the-bronx/10451",
+        "OAK": "ca/oakland/94621",
+        "SEA": "wa/seattle/98134",
+        "TBR": "fl/st.-petersburg/33705",
+        "TEX": "tx/arlington/76011",
+        "TOR": "ny/buffalo/14203",
+        "ARI": "az/phoenix/85004",
+        "ATL": "ga/atlanta/30339",
+        "CHC": "il/chicago/60613",
+        "CIN": "oh/cincinnati/45202",
+        "COL": "co/denver/80205",
+        "MIA": "fl/miami/33125",
+        "HOU": "tx/houston/77002",
+        "LAD": "ca/los-angeles/90012",
+        "MIL": "wi/milwaukee/53214",
+        "WSN": "dc/washington/20003",
+        "NYM": "ny/queens/11368",
+        "PHI": "pa/philadelphia/19148",
+        "PIT": "pa/pittsburgh/15212",
+        "STL": "mo/st.-louis/63102",
+        "SDP": "ca/san-diego/92101",
+        "SFG": "ca/san-francisco/94107",
+    }
+    return wunderground_dict[team]
+
+
 class MlbForecastScraper(WundergroundScraper):
     def get_resource(self, args):
-        endpoint = f"hourly/us/oh/cleveland/44115"
+        team = args["team"]
+        date = args["date"]
+        team_endpoint = get_team_endpoint(team)
+        endpoint = f"hourly/us/{team_endpoint}/date/{date}"
         self.get(endpoint)
         self.driver.implicitly_wait(15)
         forecast_table = self.find_element("#hourly-forecast-table")
--- mlb/app/lib/database/builder/forecasts.rb
+++ mlb/app/lib/database/builder/forecasts.rb
@@ -7,17 +7,14 @@ module Database
 
 
       def build
-        dates = [Date.yesterday, Date.today, Date.tomorrow]
+        dates = [Date.today, Date.tomorrow]
         dates.each do |date|
           puts "Building Forecasts for Date #{date}"
           @season.games.where(date: date).each do |game|
             next unless needs_data?(game)
   
             puts "Building Forecasts for Game #{game.id}"
-            team = game.home_team.abbr
-            date = game.date.strftime("%Y%m%d")
-  
-            forecast_res = query_server(:forecasts, team: team, date: date)
+            forecast_res = query_server(:forecasts, team: game.home_team.abbr, date: game.date)
             forecasts = forecast_res["forecasts"]
             forecasts.each do |forecast_data|
               forecast = Forecast.find_or_create_by(season: @season, game: game, time: forecast_data["time"])
