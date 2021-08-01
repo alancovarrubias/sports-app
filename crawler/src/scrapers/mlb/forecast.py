5fd1a028bd26de352d3a63459a9a73da16e88d90
Alan Covarrubias <alan.cov@icloud.com>
2021-08-01 09:35:55 -0700

    fix forecasts

--- crawler/src/scrapers/mlb/forecast.py
+++ crawler/src/scrapers/mlb/forecast.py
@@ -4,36 +4,36 @@ from models.mlb.forecast import MlbForecast
 
 def get_team_endpoint(team):
     wunderground_dict = {
-        "LAA": "ca/anaheim/92806",
-        "BAL": "md/baltimore/21201",
-        "BOS": "ma/boston/02215",
-        "CHW": "il/chicago/60616",
-        "CLE": "oh/cleveland/44115",
-        "DET": "mi/detroit/48201",
-        "KCR": "mo/kansas-city/64129",
-        "MIN": "mn/minneapolis/55403",
-        "NYY": "ny/the-bronx/10451",
-        "OAK": "ca/oakland/94621",
-        "SEA": "wa/seattle/98134",
-        "TBR": "fl/st.-petersburg/33705",
-        "TEX": "tx/arlington/76011",
+        "LAA": "us/ca/anaheim/92806",
+        "BAL": "us/md/baltimore/21201",
+        "BOS": "us/ma/boston/02215",
+        "CHW": "us/il/chicago/60616",
+        "CLE": "us/oh/cleveland/44115",
+        "DET": "us/mi/detroit/48201",
+        "KCR": "us/mo/kansas-city/64129",
+        "MIN": "us/mn/minneapolis/55403",
+        "NYY": "us/ny/the-bronx/10451",
+        "OAK": "us/ca/oakland/94621",
+        "SEA": "us/wa/seattle/98134",
+        "TBR": "us/fl/st.-petersburg/33705",
+        "TEX": "us/tx/arlington/76011",
         "TOR": "ca/toronto",
-        "ARI": "az/phoenix/85004",
-        "ATL": "ga/atlanta/30339",
-        "CHC": "il/chicago/60613",
-        "CIN": "oh/cincinnati/45202",
-        "COL": "co/denver/80205",
-        "MIA": "fl/miami/33125",
-        "HOU": "tx/houston/77002",
-        "LAD": "ca/los-angeles/90012",
-        "MIL": "wi/milwaukee/53214",
-        "WSN": "dc/washington/20003",
-        "NYM": "ny/queens/11368",
-        "PHI": "pa/philadelphia/19148",
-        "PIT": "pa/pittsburgh/15212",
-        "STL": "mo/st.-louis/63102",
-        "SDP": "ca/san-diego/92101",
-        "SFG": "ca/san-francisco/94107",
+        "ARI": "us/az/phoenix/85004",
+        "ATL": "us/ga/atlanta/30339",
+        "CHC": "us/il/chicago/60613",
+        "CIN": "us/oh/cincinnati/45202",
+        "COL": "us/co/denver/80205",
+        "MIA": "us/fl/miami/33125",
+        "HOU": "us/tx/houston/77002",
+        "LAD": "us/ca/los-angeles/90012",
+        "MIL": "us/wi/milwaukee/53214",
+        "WSN": "us/dc/washington/20003",
+        "NYM": "us/ny/queens/11368",
+        "PHI": "us/pa/philadelphia/19148",
+        "PIT": "us/pa/pittsburgh/15212",
+        "STL": "us/mo/st.-louis/63102",
+        "SDP": "us/ca/san-diego/92101",
+        "SFG": "us/ca/san-francisco/94107",
     }
     return wunderground_dict[team]
 
@@ -43,7 +43,8 @@ class MlbForecastScraper(WundergroundScraper):
         team = args["team"]
         date = args["date"]
         team_endpoint = get_team_endpoint(team)
-        endpoint = f"hourly/us/{team_endpoint}/date/{date}"
+        endpoint = f"hourly/{team_endpoint}/date/{date}"
+        print(endpoint)
         self.get(endpoint)
         self.driver.implicitly_wait(15)
         forecast_table = self.find_element("#hourly-forecast-table")
