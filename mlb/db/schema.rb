c8811e4f3ab0373579a46f67ebbf0c7db74bc717
Alan Covarrubias <alan.cov@icloud.com>
2021-08-01 09:08:47 -0700

    add schema

--- mlb/db/schema.rb
+++ mlb/db/schema.rb
@@ -10,9 +10,134 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_07_29_042943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "batting_stats", force: :cascade do |t|
+    t.string "interval_type"
+    t.bigint "interval_id"
+    t.string "model_type"
+    t.bigint "model_id"
+    t.boolean "starter"
+    t.integer "ab"
+    t.integer "r"
+    t.integer "h"
+    t.integer "rbi"
+    t.integer "bb"
+    t.integer "so"
+    t.integer "pa"
+    t.float "ba"
+    t.float "obp"
+    t.float "slg"
+    t.float "ops"
+    t.integer "fb"
+    t.integer "ld"
+    t.integer "gb"
+    t.integer "fb_p"
+    t.integer "ld_p"
+    t.integer "gb_p"
+    t.index ["interval_type", "interval_id"], name: "index_batting_stats_on_interval_type_and_interval_id"
+    t.index ["model_type", "model_id"], name: "index_batting_stats_on_model_type_and_model_id"
+  end
+
+  create_table "forecasts", force: :cascade do |t|
+    t.bigint "season_id"
+    t.bigint "game_id"
+    t.string "time"
+    t.string "conditions"
+    t.integer "temp"
+    t.integer "dew"
+    t.integer "humidity"
+    t.string "wind"
+    t.float "pressure"
+    t.index ["game_id"], name: "index_forecasts_on_game_id"
+    t.index ["season_id"], name: "index_forecasts_on_season_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.bigint "season_id"
+    t.bigint "away_team_id"
+    t.bigint "home_team_id"
+    t.date "date"
+    t.integer "num"
+    t.time "time"
+    t.index ["away_team_id"], name: "index_games_on_away_team_id"
+    t.index ["home_team_id"], name: "index_games_on_home_team_id"
+    t.index ["season_id"], name: "index_games_on_season_id"
+  end
+
+  create_table "lines", force: :cascade do |t|
+    t.bigint "season_id"
+    t.bigint "game_id"
+    t.string "bookie"
+    t.float "total"
+    t.index ["game_id"], name: "index_lines_on_game_id"
+    t.index ["season_id"], name: "index_lines_on_season_id"
+  end
+
+  create_table "pitching_stats", force: :cascade do |t|
+    t.string "interval_type"
+    t.bigint "interval_id"
+    t.string "model_type"
+    t.bigint "model_id"
+    t.boolean "is_starter", default: false
+    t.float "ip"
+    t.integer "h"
+    t.integer "r"
+    t.integer "er"
+    t.integer "bb"
+    t.integer "so"
+    t.integer "hr"
+    t.float "era"
+    t.integer "fb"
+    t.integer "ld"
+    t.integer "gb"
+    t.integer "fb_p"
+    t.integer "ld_p"
+    t.integer "gb_p"
+    t.index ["interval_type", "interval_id"], name: "index_pitching_stats_on_interval_type_and_interval_id"
+    t.index ["model_type", "model_id"], name: "index_pitching_stats_on_model_type_and_model_id"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.bigint "season_id"
+    t.bigint "team_id"
+    t.string "name"
+    t.string "abbr"
+    t.string "position"
+    t.integer "age"
+    t.index ["season_id"], name: "index_players_on_season_id"
+    t.index ["team_id"], name: "index_players_on_team_id"
+  end
+
+  create_table "preds", force: :cascade do |t|
+    t.bigint "season_id"
+    t.bigint "game_id"
+    t.string "desc"
+    t.float "away_score"
+    t.float "home_score"
+    t.index ["game_id"], name: "index_preds_on_game_id"
+    t.index ["season_id"], name: "index_preds_on_season_id"
+  end
+
+  create_table "seasons", force: :cascade do |t|
+    t.integer "year"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.bigint "season_id"
+    t.string "name"
+    t.string "city"
+    t.string "abbr"
+    t.string "link"
+    t.index ["season_id"], name: "index_teams_on_season_id"
+  end
+
+  create_table "weathers", force: :cascade do |t|
+    t.bigint "game_id"
+    t.index ["game_id"], name: "index_weathers_on_game_id"
+  end
 
 end
