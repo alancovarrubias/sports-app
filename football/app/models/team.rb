99351043604ac15b56083c4b1e3116f830ef68b5
Alan Covarrubias <alan.cov@icloud.com>
2022-04-28 22:57:10 -0700

    add uniqueness and presence validation

--- football/app/models/team.rb
+++ football/app/models/team.rb
@@ -1,3 +1,5 @@
 class Team < ApplicationRecord
   belongs_to :season
+  validates :name, presence: true, uniqueness: { scope: :season_id }
+  validates :abbr, presence: true, uniqueness: { scope: :season_id }
 end
