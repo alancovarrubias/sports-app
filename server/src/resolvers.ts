19e0eb7595716a7e9b45e56aa53495b69d39edbf
Alan Covarrubias <alan.cov@icloud.com>
2023-08-28 21:48:45 -0700

    combine auth

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -2,30 +2,28 @@ import { GraphQLError } from "graphql";
 import { Resolvers } from "@app/__generated__/resolvers-types";
 import { GAME } from "@test-utils/mocks";
 
+const withAuthentication = (resolverFunction) => {
+  return (root, args, context) => {
+    if (!context.user) {
+      throw new GraphQLError('User is not authenticated', {
+        extensions: {
+          code: 'UNAUTHENTICATED',
+          http: { status: 401 },
+        },
+      });
+    }
+    return resolverFunction(root, args, context);
+  };
+};
+
 const resolvers: Resolvers = {
   Query: {
-    currentUser: (_root, _args, { user }) => {
-      if (!user) {
-        throw new GraphQLError("User is not authenticated", {
-          extensions: {
-            code: "UNAUTHENTICATED",
-            http: { status: 401 },
-          },
-        });
-      }
+    currentUser: withAuthentication((_root, _args, { user }) => {
       return user;
-    },
-    games: (_root, _args, { user }) => {
-      if (!user) {
-        throw new GraphQLError("User is not authenticated", {
-          extensions: {
-            code: "UNAUTHENTICATED",
-            http: { status: 401 },
-          },
-        });
-      }
+    }),
+    games: withAuthentication(() => {
       return [GAME]
-    }
+    })
   },
   Mutation: {
     login: async (_root, args, { dataSources: { authAPI } }) => {
