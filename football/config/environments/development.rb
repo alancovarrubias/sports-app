f440f80d6b1621a12ec81d4f8fbc3609790042ee
Alan Covarrubias <alan.cov@icloud.com>
2023-09-14 00:16:20 -0700

    add football hosts to dev

--- football/config/environments/development.rb
+++ football/config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.hosts << 'football'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -41,7 +42,6 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
