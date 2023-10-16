a67cdb1bfd6bc6a2668410602897485f215eafcc
Alan Covarrubias <alan.cov@icloud.com>
2023-10-16 15:35:28 -0700

    initial sidekiq commit

--- football/config/routes.rb
+++ football/config/routes.rb
@@ -1,3 +1,7 @@
+require 'sidekiq/web'
+Sidekiq::Web.use ActionDispatch::Cookies
+Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_interslice_session'
 Rails.application.routes.draw do
   resources :games, only: %i[index show]
+  mount Sidekiq::Web => '/sidekiq'
 end
