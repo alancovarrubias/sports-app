b0ecaaa239888f53d6c1f933166ce2b86ef13f5c
Alan Covarrubias <alan.cov@icloud.com>
2023-10-17 16:58:18 -0700

    reduce threads for sidekiq and db

--- football/config/initializers/sidekiq.rb
+++ football/config/initializers/sidekiq.rb
@@ -4,6 +4,7 @@ Sidekiq.configure_server do |config|
     schedule_file = 'config/schedule.yml'
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
   end
+  config.options[:concurrency] = 3
 end
 
 Sidekiq.configure_client do |config|
