47875709460364828620016bfa21f1c4e337a2cd
Alan Covarrubias <aecovarru@gmail.com>
2024-09-29 16:13:16 -0700

    disable gpu in crawler

--- crawler/src/v2/scrapers/base_scraper.py
+++ crawler/src/v2/scrapers/base_scraper.py
@@ -25,6 +25,7 @@ class BaseScraper(ABC):
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
         chrome_options.page_load_strategy = "eager"
         return webdriver.Chrome(options=chrome_options)
     
