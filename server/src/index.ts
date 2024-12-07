ead7276892f37e8969de14c79e9a9c10fc99110c
Alan Covarrubias <aecovarru@gmail.com>
2024-12-07 08:26:35 +0000

    wip

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,50 @@
-import runServer from "@app/runServer";
+// npm install @apollo/server express graphql cors
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import resolvers from "@app/resolvers";
+import { readFileSync } from "fs";
+const typeDefs = readFileSync("./schema.graphql", { encoding: "utf-8" });
 
-const { url } = await runServer();
-console.log(`🚀  Server ready at: ${url}`);
+interface MyContext {
+    token?: string;
+}
+
+// Required logic for integrating with Express
+const app = express();
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
+
+// Same ApolloServer initialization as before, plus the drain plugin
+// for our httpServer.
+const server = new ApolloServer<MyContext>({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+// Ensure we wait for our server to start
+await server.start();
+
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+    '/',
+    cors<cors.CorsRequest>(),
+    express.json(),
+    // expressMiddleware accepts the same arguments:
+    // an Apollo Server instance and optional configuration options
+    expressMiddleware(server, {
+        context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+);
+
+// Modified server startup
+await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve),
+);
+console.log(`🚀 Server ready at http://localhost:4000/`);
