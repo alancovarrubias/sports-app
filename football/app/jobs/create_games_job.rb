3add78167477b2275433e7499435888a308326c9
Alan Covarrubias <aecovarru@gmail.com>
2024-12-18 04:59:36 +0000

    update validators

--- football/app/jobs/create_games_job.rb
+++ football/app/jobs/create_games_job.rb
@@ -4,7 +4,7 @@ class CreateGamesJob < ApplicationJob
   def perform
     LEAGUES.each do |league|
       schedule_data = Crawler.schedule(league: league)
-      season = Season.find_or_create_by(year: schedule_data[:year], league: league)
+      season = Season.find_or_create_by!(year: schedule_data[:year], league: league)
       schedule_data[:espn_ids].each do |espn_id|
         UpdateGameJob.perform_later(espn_id, season.id)
       end
