72546fe38d2b5f5f6c3a511dd84e5aab1ba9bd1b
Alan Covarrubias <aecovarru@gmail.com>
2024-12-30 22:34:28 -0800

    don't create games if already created

--- football/app/jobs/create_games_job.rb
+++ football/app/jobs/create_games_job.rb
@@ -6,6 +6,8 @@ class CreateGamesJob < ApplicationJob
       schedule_data = Crawler.schedule(league: league)
       season = Season.find_or_create_by!(year: schedule_data[:year], league: league)
       schedule_data[:espn_ids].each do |espn_id|
+        next if Game.find_by_espn_id(espn_id)
+
         UpdateGameJob.perform_later(espn_id, season.id, schedule_data[:week])
       end
     end
