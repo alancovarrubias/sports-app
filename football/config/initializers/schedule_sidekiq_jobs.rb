ec1e8a90e2834fff0635e13d1848f2fd578f58f6
Alan Covarrubias <aecovarru@gmail.com>
2024-12-31 11:00:49 -0800

    move job initializer to seed file

--- football/config/initializers/schedule_sidekiq_jobs.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.after_initialize do
-  ScheduleUpdateGamesJob.perform_later
-  ScheduleLinesJob.perform_later
-  CreateGamesJob.perform_later
-end
--- football/db/seeds.rb
+++ football/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+CreateGamesJob.perform_later
+ScheduleUpdateGamesJob.perform_later
+ScheduleLinesJob.perform_later
