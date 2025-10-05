0b29a508da2a2fab7e8b4e7f3efb1258e5b9909c
Alan Covarrubias <aecovarru@gmail.com>
2025-10-05 23:58:54 +0000

    wip

--- crawler/src/v2/scrapers/play_by_play.py
+++ crawler/src/v2/scrapers/play_by_play.py
@@ -2,10 +2,11 @@ from v2.scrapers.base import BaseScraper
 
 class PlayByPlayScraper(BaseScraper):
     def parse_data(self, finished):
+        print(self.driver.current_url)
         return {
-            "received": self.get_received(0 if finished else -1)
+            "received": self.get_received(1)
         }
 
     def get_received(self, logo_index):
-        team_logos = self.find_elements(".Gamestrip__Logo")
+        team_logos = self.find_element("div[role='tabpanel']").find_elements("img")
         return team_logos[logo_index].get_attribute("alt")
