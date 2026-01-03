b803efefc3ff145c02e757f56444d44d4c3a020c
Alan Covarrubias <aecovarru@gmail.com>
2026-01-02 23:55:10 -0800

    add sidekiq concurrency var

--- football/config/initializers/sidekiq.rb
+++ football/config/initializers/sidekiq.rb
@@ -6,7 +6,7 @@ Sidekiq.configure_server do |config|
     schedule_file = 'config/schedule.yml'
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
   end
-  config.options[:concurrency] = 2
+  config.options[:concurrency] = Integer(ENV.fetch('SIDEKIQ_CONCURRENCY', 5))
 
   config.client_middleware do |chain|
     chain.add SidekiqUniqueJobs::Middleware::Client
