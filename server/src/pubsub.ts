25c2878cfa77f554f14a60c2cddf239da7312bb3
Alan Covarrubias <aecovarru@gmail.com>
2026-09-14 22:43:01 -0700

    Update redis to read from env var

--- server/src/pubsub.ts
+++ server/src/pubsub.ts
@@ -1,11 +1,7 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import Redis from 'ioredis';
 
-const redisOptions = {
-    host: 'redis',
-    port: 6379,
-};
-const redisClient = new Redis(redisOptions);
+const redisClient = new Redis(process.env.REDIS_URL ?? 'redis://redis:6379');
 const pubsub = new RedisPubSub({
     publisher: redisClient,
     subscriber: redisClient,
